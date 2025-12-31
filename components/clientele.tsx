b7b44154f707952829d4b8d6ec788c564739b3a3
Pranav Soni <pranavsoni1101@gmail.com>
2025-12-31 15:54:05 +0530

    Updated clientele section

--- components/clientele.tsx
+++ components/clientele.tsx
@@ -4,17 +4,17 @@ import { Container } from "@/components/container";
 const brands = [
   { name: "Case Construction", logo: "/case-construction-equipment-logo-black.svg" },
   { name: "Eicher", logo: "/eicher.svg" },
+  { name: "Metalman", logo: "/metalman-logo.svg" },
+  { name: "John Deere", logo: "/john-deere.svg" },
   { name: "Cummins", logo: "/ctt.png" },
   { name: "Methodex", logo: "/methodex.png" },
   { name: "VE Commercial Vehicles", logo: "/ve-commercial-vehicles.png" },
   { name: "Jash", logo: "/jash.png" },
-  { name: "Metalman", logo: "/metalman-logo.svg" },
   { name: "Caparo", logo: "/caparo.png" },
   { name: "Erawat Pharma", logo: "/erawat.png" },
-  { name: "Fort Caps", logo: "/fort-caps.png" },
   { name: "Diversitech", logo: "/diversitech.png" },
   { name: "Gatiman Auto", logo: "/gatiman.png" },
-  { name: "John Deere", logo: "/john-deere.svg" },
+  { name: "Fort Caps", logo: "/fort-caps.png" },
 ];
 
 const BrandShowcase = () => {
@@ -29,22 +29,31 @@ const BrandShowcase = () => {
           construction, and industrial sectors.
         </p>
 
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-8 items-center justify-items-center">
-          {brands.map((brand, idx) => (
-            <div
-              key={idx}
-              className="grayscale hover:grayscale-0 transition-all duration-300"
-            >
-              <Image
-                src={brand.logo}
-                alt={brand.name}
-                width={160}
-                height={80}
-                className="object-contain"
-              />
-            </div>
-          ))}
-        </div>
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-8 items-center">
+  {brands.map((brand, idx) => {
+    const isLast = idx === brands.length - 1;
+
+    return (
+      <div
+        key={idx}
+        className={[
+          "justify-self-center grayscale hover:grayscale-0 transition-all duration-300",
+          // On md:grid-cols-4, make the last item sit in the middle
+          isLast ? "md:col-start-2 md:col-span-2" : "",
+        ].join(" ")}
+      >
+        <Image
+          src={brand.logo}
+          alt={brand.name}
+          width={160}
+          height={80}
+          className="object-contain"
+        />
+      </div>
+    );
+  })}
+</div>
+
       </Container>
     </section>
   );
